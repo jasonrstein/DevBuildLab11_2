9c42ad9d8d225a84fb125a407ba47e58f6932f9e
Jason Stein <jasonstein@quickenloans.com>
2021-05-17 09:06:14 -0400

    Lab 2 continued to Lab 3

--- DevBuild11_2/Controllers/HomeController.cs
+++ DevBuild11_2/Controllers/HomeController.cs
@@ -28,22 +28,25 @@ namespace DevBuild11_2.Controllers
             return View();
         }
 
-        public IActionResult RegistrationConfirmed(string lastname, string firstname, string password, string email, string gender, string prefix)
+        public IActionResult RegistrationConfirmed(User user)
         {
-            if (password.Length < 8)
+            if (user.password.Length < 8)
             {
                 return Content("Password is not long enough");
             }
 
-            ViewData["FirstName"] = firstname;
-            ViewData["LastName"] = lastname;
-            ViewData["email"] = email;
-            ViewData["gender"] = gender;
-            ViewData["Prefix"] = prefix;
-            return Content($"Thank you {prefix}{firstname} {lastname} for registering. "
-                           + $"We have your gender listed as {gender}.\n"
-                           + "You will be contacted my a ninja assasin with future details.\n"
-                           + "If the assassin does not kill you, then you have made it to the second round interview.");
+            //ViewData["FirstName"] = firstname;
+            //ViewData["LastName"] = lastname;
+            //ViewData["email"] = email;
+            //ViewData["gender"] = gender;
+            //ViewData["Prefix"] = prefix;
+            //ViewData["telephone"] = telephone;
+            //return Content($"Thank you {prefix}{firstname} {lastname} for registering. "
+            //               + $"We have your gender listed as {gender} and your origin of abilites as {origin}.\n"
+            //               + "You will be contacted my a ninja assasin with future details.\n"
+            //               + "If the assassin does not kill you, then you have made it to the second round interview.");
+
+            return View(user);
         }
 
         public IActionResult Privacy()
---
+++ DevBuild11_2/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevBuild11_2.Models
+{
+    public class User
+    { 
+        public string lastName { get; set; }
+        public string firstName { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+        public string telephone { get; set; }
+        public string gender { get; set; }
+        public string origin { get; set; }
+    }
+}
